16b79a236e9291e6030e3f3dc41dd3353cc39d8a
Daniel Cox <coxdaniel88@gmail.com>
2016-04-05 23:57:11 -0400

    Initial commit - basic game elements for a Pong clone in Unity2D. Based on a tutorial from noobtuts (http://noobtuts.com/unity/2d-pong-game), to be expanded upon.
    - Physical paddles controlled by W/S (P1) UP/DOWN (P2).
    - Ball returns to center and moves toward loser at half-speed.
    
    To do:
    
    Score
    Better ball destroy/respawn
    Point limit
    AI opponent

---
+++ PaddleBounce/Assets/BallMove.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallMove : MonoBehaviour {
+    public float speed = 15;
+
+	void Start () {
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        //Left Paddle
+        if (col.gameObject.name == "PaddleA")
+        {
+            float y = hitFactor(transform.position, col.transform.position, col.collider.bounds.size.y);
+
+            Vector2 dir = new Vector2(1, y).normalized;
+
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+        //Right Paddle
+        if (col.gameObject.name == "PaddleB")
+        {
+            float y = hitFactor(transform.position, col.transform.position, col.collider.bounds.size.y);
+
+            Vector2 dir = new Vector2(-1, y).normalized;
+
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+
+        if(col.gameObject.name == "WallLeft")
+        {
+            //yield return new WaitForSeconds(1);
+            transform.position = new Vector3(0, 0, 0);
+            Vector2 dir = new Vector2(-1, 0).normalized;
+
+            GetComponent<Rigidbody2D>().velocity = dir * (speed/2);
+        }
+        if (col.gameObject.name == "WallRight")
+        {
+            //yield return new WaitForSeconds(1);
+            transform.position = new Vector3(0, 0, 0);
+            Vector2 dir = new Vector2(1, 0).normalized;
+
+            GetComponent<Rigidbody2D>().velocity = dir * (speed/2);
+        }
+    }
+
+    float hitFactor(Vector2 ballPos, Vector2 paddlePos, float paddleHeight)
+    {
+        return (ballPos.y - paddlePos.y) / paddleHeight;
+    }
+}
---
+++ PaddleBounce/Assets/PaddleMove.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleMove : MonoBehaviour {
+    public float speed = 10;
+    public string axis = "Vertical";
+	void FixedUpdate()
+    {
+        float v = Input.GetAxisRaw(axis);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, v) * speed;
+    }
+}
